26af78bb7543095d04aa29c036c75853f6d9cd73
TddSamaNoMercy <xymreasonxxs@163.com>
2019-10-02 08:49:02 +0800

    v1.03

--- src/com/xym/pro01/Main.java
+++ src/com/xym/pro01/Main.java
@@ -1,20 +1,40 @@
 package com.xym.pro01;
 
+import javax.jws.soap.SOAPBinding;
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
 
     public static void main(String... args) {
         int i = 0;
-        if (i!=1) System.out.println("test master");
+        if (i != 1) System.out.println("test master");
         System.out.println("test editor");
 
         System.out.println("test .git ignore");
+
+        User[] names = {new User(01, "dada"), new User(02, "dad")
+                , new User(03, "dssssada")};
+        Arrays.sort(names, Comparator.comparing(User::getId));
+        Arrays.sort(names, Comparator.comparingInt(o -> o.getName().length()));
+//        Arrays.sort(names, new Comparator<User>() {
+//            @Override
+//            public int compare(User o1, User o2) {
+//                return o1.getId() - o2.getId();
+//            }
+//        });
+//        names.sort(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.length() - o2.length();
+//            }
+//        });
+//        names.sort(Comparator.comparingInt(String::length));
+        System.out.println(Arrays.toString(names));
+
 //        int i = 1;
 //        new Timer(1000, e -> {
 //            System.out.println("At the tone, the time is " + i +  new Date());
@@ -29,7 +49,7 @@ public class Main {
 //        System.out.println(cat.equals(cat1));
 
         System.out.println(Arrays.toString(args));
-        java.util.List<String> names = new ArrayList<>();
+//        java.util.List<String> names = new ArrayList<>();
 //        names.contains()
 //        names.removeIf(new Predicate<String>() {
 //            @Override
---
+++ src/com/xym/pro01/User.java
@@ -0,0 +1,52 @@
+package com.xym.pro01;
+
+import java.util.Objects;
+
+public class User implements Comparable<User>{
+    private int id;
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return name.length() - o.getName().length();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
